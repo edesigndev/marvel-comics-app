804b0dd1d42043ba2aa12f1140d7f0059ce392a1
Edgar <edgar.lazaro23@gmail.com>
2024-12-10 20:09:36 -0500

    change style page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default function Home() {
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {comics.map((comic) => (
           <div
-            className="group relative block bg-black rounded-md overflow-hidden shadow-md"
+            className="group relative block bg-black rounded-md overflow-hidden shadow-md h-96"
             key={comic.id}
           >
             <Image
@@ -55,14 +55,12 @@ export default function Home() {
               className="absolute inset-0 h-full w-full object-cover opacity-75 transition-opacity group-hover:opacity-50"
             />
 
-            <div className="relative p-4 sm:p-6 lg:p-8">
-              <p className="text-sm font-medium uppercase tracking-widest text-pink-500"></p>
-              <div className="mt-32 sm:mt-48 lg:mt-64">
-                <div className="translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100">
-                  <p className="text-sm text-white">{comic.title}</p>
-                </div>
+            <div className="flex flex-col relative p-2">
+              <div className="mt-72 translate-y-8 transform opacity-0 transition-all group-hover:translate-y-0 group-hover:opacity-100 h-10">
+                <p className="text-sm text-white">{comic.title}</p>
               </div>
-              <div className="flex flex-row gap-2">
+
+              <div className="flex flex-row justify-end gap-2">
                 <ShareButton
                   imageUrl={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
                 />
